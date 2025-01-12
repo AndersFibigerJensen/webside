715a8e41e70889c7485a0ec3f0be4f5b364c90bd
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2025-01-12 11:30:22 +0100

    hello

---
+++ transaction.js
@@ -0,0 +1,28 @@
+const baseurl=""
+
+Vue.createApp({
+    data()
+    {
+        return {vej:product_category=0,Transaction_qty:0, store_location:""}
+
+    },
+    methods:
+    {
+        async getid(id)
+        {
+
+        },
+        async deleteid(id)
+        {
+
+        },
+        async updateid(id)
+        {
+
+        },
+        async addtransaction()
+        {
+
+        }
+    }
+}).mount("#app")
